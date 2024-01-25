2da886d27d731cac0a0d679eb1cab6be4092c074
rijalsujan09 <srijal@castera.io>
2024-01-25 17:00:48 -0500

    some more about ts'

--- public/src/app.js
+++ public/src/app.js
@@ -12,12 +12,55 @@ const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    let values;
+    values = [toForm.value, details.value, amount.valueAsNumber];
     let doc;
     if (type.value === 'invoice') {
-        doc = new Invoice(toForm.value, details.value, amount.valueAsNumber);
+        // doc = new Invoice(toForm.value, details.value, amount.valueAsNumber);
+        doc = new Invoice(...values);
     }
     else {
         doc = new Payment(toForm.value, details.value, amount.valueAsNumber);
     }
     list.render(doc, type.value, 'end');
 });
+// GENERICS 
+const addUId = (obj) => {
+    let uid = Math.floor(Math.random() * 100);
+    return Object.assign(Object.assign({}, obj), { uid });
+};
+let docOne = addUId({ name: 'sujan', age: 25 });
+console.log(docOne.age);
+// interface
+// ENUMS
+var ResourceType;
+(function (ResourceType) {
+    ResourceType[ResourceType["BOOK"] = 0] = "BOOK";
+    ResourceType[ResourceType["AUTHOR"] = 1] = "AUTHOR";
+    ResourceType[ResourceType["FILM"] = 2] = "FILM";
+    ResourceType[ResourceType["DIRECTOR"] = 3] = "DIRECTOR";
+    ResourceType[ResourceType["PERSON"] = 4] = "PERSON";
+})(ResourceType || (ResourceType = {}));
+const doc3 = {
+    uid: 1,
+    resourceName: ResourceType.AUTHOR,
+    data: new Date(),
+};
+const doc4 = {
+    uid: 2,
+    resourceName: ResourceType.DIRECTOR,
+    data: ["bread", "milk"]
+};
+console.log(doc3, doc4);
+// TUPLES
+let arr = ['ryu', 25, true];
+arr[0] = false;
+arr[1] = 'yoshi';
+arr = [30, false, 'yoshi'];
+let tup = ['sujan', 25, true];
+tup[0] = "rijal";
+tup[1] = 25;
+tup[2] = true;
+let student;
+// student = [12345, "chun-li"]
+student = ['chun-li', 225];
--- src/app.ts
+++ src/app.ts
@@ -17,10 +17,14 @@ const amount = document.querySelector('#amount') as HTMLInputElement;
 form.addEventListener('submit', (e:Event) => {
     e.preventDefault();
 
+    let values :[string, string, number]
+     values = [toForm.value, details.value, amount.valueAsNumber];
+
     let doc: HasFormater;
 
     if(type.value === 'invoice'){
-        doc = new Invoice(toForm.value, details.value, amount.valueAsNumber);
+        // doc = new Invoice(toForm.value, details.value, amount.valueAsNumber);
+        doc = new Invoice(...values);
     }else{
         doc = new Payment(toForm.value, details.value, amount.valueAsNumber);
     }
@@ -28,3 +32,69 @@ form.addEventListener('submit', (e:Event) => {
     list.render(doc, type.value, 'end')
     
 })
+
+
+// GENERICS 
+
+const addUId  = <T extends {name:string, age:number,}>(obj: T) => {
+    let uid = Math.floor(Math.random()*100);
+    return {...obj, uid};
+}
+let docOne = addUId({name:'sujan', age:25})
+
+console.log(docOne.age)
+
+// interface
+
+// ENUMS
+enum ResourceType {
+    BOOK,
+    AUTHOR,
+    FILM,
+    DIRECTOR,
+    PERSON
+}
+
+
+interface Resource <T>{
+    uid:number,
+    resourceName:ResourceType,
+    data: T;
+}
+
+
+const doc3 :Resource<Date> ={
+    uid:1,
+    resourceName:ResourceType.AUTHOR,
+    data: new Date(),
+}
+
+
+const doc4 : Resource<string[]> ={
+    uid:2,
+    resourceName:ResourceType.DIRECTOR,
+    data : ["bread", "milk"]
+}
+
+console.log(doc3, doc4)
+
+
+// TUPLES
+
+let  arr = ['ryu', 25, true];
+arr[0]=false;
+arr[1]='yoshi';
+arr = [30, false, 'yoshi']
+
+
+let tup: [string, number, boolean] = ['sujan', 25, true]
+tup[0] = "rijal"
+tup[1] = 25;
+tup[2] = true;
+
+
+let student : [string, number];
+// student = [12345, "chun-li"]
+student = ['chun-li', 225]
+
+
